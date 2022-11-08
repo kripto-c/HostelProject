b837f989f2c5024c171d8408bd8650df05c7dccd
kripto-c <mgutierrezxred@gmail.com>
2022-11-08 14:19:14 -0400

    add login and route

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,23 @@
 import './App.css';
+import { Routes, Route } from "react-router-dom";
+import Login from './Components/login/Login';
+import Home from './Components/home/home';
+
+
 import {BrowserRouter, Route, Routes} from "react-router-dom"
 import 'bootstrap/dist/css/bootstrap.min.css'
 import { useAuth0 } from '@auth0/auth0-react';
 
 function App() {
-  const { loginWithPopup, loginWithRedirect, logout, user, isAuthenticated, getAccessTokenSilently } = useAuth0();
 
   return (
-    <BrowserRouter>
-      <div>
+      <div className='App'>
         <Routes>
-    
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/login" element={<Login />}></Route>
         </Routes>
+
       </div>
-    </BrowserRouter>
   );
 }
 
