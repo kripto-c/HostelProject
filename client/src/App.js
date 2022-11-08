3625f4ce64ce1bf38ad3c6d115b12db397abcbb9
kripto-c <mgutierrezxred@gmail.com>
2022-11-08 17:49:21 -0400

    error

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
-import { useAuth0 } from '@auth0/auth0-react';
-import axios from 'axios';
+import { Routes, Route } from "react-router-dom";
+import Login from './Components/login/Login';
+import Home from './Components/home/home';
+
 
 function App() {
-  const { loginWithPopup, loginWithRedirect, logout, user, isAuthenticated, getAccessTokenSilently } = useAuth0();
 
   return (
       <div className='App'>
