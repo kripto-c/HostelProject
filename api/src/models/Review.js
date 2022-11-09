949687ae6542687f473ec65a0800e22286d513fa
galoss88 <gallogarcianicolas@gmail.com>
2022-11-09 00:30:33 -0300

    model review

---
+++ api/src/models/Review.js
@@ -0,0 +1,20 @@
+const { DataTypes } = require("sequelize");
+module.exports = (sequelize) => {
+  sequelize.define("review", {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      allowNull: false,
+      primaryKey: true,
+    },
+    rating: {
+      type:DataTypes.INTEGER,
+      allowNull:false,
+
+    },
+    description:{
+        type:DataTypes.STRING,
+        allowNull:false,
+    }
+  });
+};
