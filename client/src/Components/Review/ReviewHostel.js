aae315497803b2d53b5c0f5e6138140f945559ad
galoss88 <gallogarcianicolas@gmail.com>
2022-11-09 04:53:41 -0300

    Update ReviewHostel.js

--- client/src/Components/Review/ReviewHostel.js
+++ client/src/Components/Review/ReviewHostel.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { useState } from "react";
 import { useDispatch } from "react-redux";
-//import BOOTSTRAP
+//import BOOTSTRAP --------------------------------------------->>
 import FloatingLabel from "react-bootstrap/FloatingLabel";
 import Form from "react-bootstrap/Form";
 import { Button } from "react-bootstrap";
