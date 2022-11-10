40d23ebcb497e4aad81de737e53acac49f714afb
german <haspenngames14@gmail.com>
2022-11-10 01:37:53 -0300

    Arreglos en el home

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,20 @@
-import './App.css';
+import "./App.css";
 import { Routes, Route } from "react-router-dom";
-import Login from './Components/login/Login';
-import Home from './Components/home/home';
-import Navbars from './Components/navbar/navbar';
-import ReviewHostel from "./Components/Review/ReviewHostel.js"
+import Login from "./Components/login/Login";
+import Home from "./Components/home/Home";
+import Navbars from "./Components/navbar/navbar";
+import ReviewHostel from "./Components/Review/ReviewHostel.js";
 
 function App() {
-
   return (
-      <>
-        <Navbars />
-        <Routes>
-         
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/reviewHostel" element={<ReviewHostel/>}/>
-  
-    
-
-        </Routes>
-
-      </>
+    <>
+      <Navbars />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/reviewHostel" element={<ReviewHostel />} />
+      </Routes>
+    </>
   );
 }
 
--- client/src/Components/home/home.jsx
+++ client/src/Components/home/home.jsx
@@ -1,9 +1,63 @@
-import React from 'react';
+import React from "react";
+import Footer from "../Layout/Footer";
+import Carousel from "react-bootstrap/Carousel";
+import "./Home.css";
 
-export default function Home() {
-     return(
-        <div>
-            home
+const Home = () => {
+  return (
+    <div className="home-container">
+      <Carousel className="carousel">
+        <Carousel.Item>
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/e/e8/Hostel_Dormitory.jpg"
+            alt="first-item"
+          />
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            src="https://media-cdn.tripadvisor.com/media/photo-s/15/bc/d3/19/ideal-social-hostel.jpg"
+            alt="second-item"
+          />
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            src="https://www.kayak.com.ar/rimg/kimg/db/d7/dc765dd2449cac1c.jpg?width=1366&height=768&crop=true"
+            alt="third-item"
+          />
+        </Carousel.Item>
+      </Carousel>
+      <h1>Bienvendios al Hostel Dinamita</h1>
+      <div className="info">
+        <div className="collage">
+        <img
+          src="https://media.gq.com.mx/photos/619fc54a7a3578ea6e576d03/1:1/w_2000,h_2000,c_limit/bares%20de%20vino-160836693.jpg"
+          alt="collage-1"
+        />
+        <img
+          src="https://cf.bstatic.com/xdata/images/xphoto/max1440/48357258.jpg?k=035ac71dc8cf908f6b770ec79bf034f5891a734d172c80159a0ac713ff2a1a7c&o="
+          alt="collage-2"
+        />
+        <img
+          src="https://i0.wp.com/www.disfrutarosario.com/wp-content/uploads/2020/10/Como-construir-una-pileta-en-casa-2.jpg?resize=640%2C480&ssl=1"
+          alt="collage-3"
+        />
+        <img
+          src="https://hqbeds.com/wp-content/uploads/2019/11/hostel1-e1573156979842.jpg"
+          alt="collage-4"
+        />
+        <img
+          src="https://www.elcalafate.tur.ar/img/anunciantes/724/matices-hostel-2-.jpg"
+          alt="collage-5"
+        />
+        <img
+          src="https://viajeros-17f81.kxcdn.com/wp-content/uploads/2020/10/DSC_0310-scaled.jpg"
+          alt="collage-6"
+        />
         </div>
-     );
-}
+      </div>
+      <Footer></Footer>
+    </div>
+  );
+};
+
+export default Home;
