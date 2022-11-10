37130a92a1ac54212d8df5eca8bdab5d2a3f4a39
kripto-c <mgutierrezxred@gmail.com>
2022-11-10 19:27:09 -0400

    route login

--- client/src/Components/navbar/navbar.jsx
+++ client/src/Components/navbar/navbar.jsx
@@ -1,25 +1,37 @@
-import React from "react";
-import Container from "react-bootstrap/Container";
-import Nav from "react-bootstrap/Nav";
-import Navbar from "react-bootstrap/Navbar";
-import NavDropdown from "react-bootstrap/NavDropdown";
-import { useAuth0 } from "@auth0/auth0-react";
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import { useAuth0 } from '@auth0/auth0-react';
+import axios from 'axios'
+
 
 function Navbars() {
-  const {
-    loginWithPopup,
-    logout,
-    user,
-    isAuthenticated,
-    getAccessTokenSilently,
-  } = useAuth0();
+  const { loginWithPopup, logout, user, isAuthenticated, getAccessTokenSilently } = useAuth0();
+
+  async function setClient () {
+    try {
+      const token = await getAccessTokenSilently();
+       const info = await axios.get('http://localhost:4000/login/setClient', {
+         headers:{
+            authorization:`Bearer ${token}`
+         },
+       })
+       console.log(info.data)
+    } catch (error) {
+       console.log(error);
+    }
+  }
+  
   return (
     <>
       <Navbar variant="dark" bg="dark">
         <Container className="d-flex justify-content-between">
-          <Navbar.Brand href="/">Dinamita Hostel</Navbar.Brand>
-          <Nav className="col-10  justify-content-end">
-            <Nav.Link href="/">Home</Nav.Link>
+          <Navbar.Brand href="/" className='col-3'>Dinamita Hostel</Navbar.Brand>
+          <Nav className="col-7 justify-content-end">
+            <Nav.Link href="/">
+              Home
+            </Nav.Link>
             <Nav.Link href="/reservas">Reservas</Nav.Link>
             <Nav.Link href="/rooms">Habitaciones</Nav.Link>
             <Nav.Link href="/contact">Contactanos</Nav.Link>
@@ -27,9 +39,12 @@ function Navbars() {
             <Nav.Link href="/reviewHostel">Reviews</Nav.Link>
           </Nav>
           {isAuthenticated ? (
-            <Navbar.Collapse id="basic-navbar-nav m-auto col-2 me-5">
+           <>
+          
+             <Navbar.Collapse className="container basic-navbar-nav m-auto col-1 ms-3">
+             <img src={isAuthenticated ? user.picture : ""} alt="foto perfil" className='rounded-circle w-25'/>
               <Nav className="me-auto">
-                <NavDropdown title="Perfil" id="basic-nav-dropdown">
+                <NavDropdown title="Mi Cuenta" id="basic-nav-dropdown">
                   <NavDropdown.Item href="#action/3.1">
                     Editar Datos
                   </NavDropdown.Item>
@@ -43,11 +58,14 @@ function Navbars() {
                 </NavDropdown>
               </Nav>
             </Navbar.Collapse>
+           </>
           ) : (
             <Navbar.Brand
-              href="/login"
-              className="col-2 text-end"
-              onClick={loginWithPopup}
+              href="/#login"
+              onClick={async ()=>{
+                await loginWithPopup()
+                 setClient()
+              }}
             >
               Login
             </Navbar.Brand>
