860f0f53a83e170553586457325ad0ece8a806c6
kripto-c <mgutierrezxred@gmail.com>
2022-11-10 19:27:09 -0400

    route login

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,5 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
-import Login from "./Components/login/Login";
 import Home from "./Components/home/home";
 import Navbars from "./Components/navbar/navbar";
 import ReviewHostel from "./Components/Review/ReviewHostel.js";
@@ -14,7 +13,6 @@ function App() {
       <Navbars />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
         <Route path="/reviewHostel" element={<ReviewHostel />} />
         <Route exact path="/rooms" element={<Rooms />} />
         <Route path="/contact" element={<Contact />} />
