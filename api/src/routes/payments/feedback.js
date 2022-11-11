7f7bcd3047cf6a43de46209146cd4be464f62bb0
LuCardozo <lucianocar12300@gmail.com>
2022-11-11 14:49:33 -0300

    correccion v2

--- api/src/routes/payments/feedback.js
+++ api/src/routes/payments/feedback.js
@@ -8,16 +8,22 @@ route.get("/", async (req, res) =>{
     const preference_id = req.query.preference_id;
     const payment_status = req.query.status;
     console.log(preference_id)
-    if(payment_status === "approved"){
-        let comprobante = await Rent.create({
-           status: true,
-           pago_id: preference_id
-        });
-        console.log(comprobante)
+    try {
+        if(payment_status === "approved"){
+            // let comprobante = await Rent.create({
+            //    status: true,
+            //    pago_id: preference_id
+            // });
+            // console.log(comprobante)
+        }
+      
+        console.log("si entro al home ponele");
+        res.send("reserva hecha");
+        
+    } catch (error) {
+        console.log(error)
+        res.send(error)
     }
-  
-    console.log("si entro al home ponele");
-    res.send("reserva hecha");
   });
 
   module.exports = route;
