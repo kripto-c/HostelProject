8504e472398dadee5a453b9a80b3f31e95ad8b5c
galoss88 <gallogarcianicolas@gmail.com>
2022-11-11 15:19:59 -0300

    cambios en seeder

--- api/src/controllers/loadInfoHostel.js
+++ api/src/controllers/loadInfoHostel.js
@@ -1,37 +1,54 @@
-// const data = require("../Info Hostel/data.js");
-const { Room, Rent, Client } = require("../db.js");
-const data = require("../Info Hostel/data")
+const data = require("../Info Hostel/data.js");
+const { Room, Rent, Client, Type } = require("../db.js");
 
 const loadInfoHostel = async (req, res) => {
   //Cargar Clientes
-  console.log(data);
   try {
     let infoDB = await Client.findAll();
     let infoDbRent = await Rent.findAll();
     let infoDbRoom = await Room.findAll();
-    if(!infoDB.length || !infoDbRent.length || !infoDbRoom.length){
-        if (!infoDB.length) {
-            let clientes = data.map((e) => e.client).flat();
-            let dataClient = await Client.bulkCreate(clientes);
-            
-          } 
-          if(!infoDbRent.length){
-              let rent = data.map(e=>e.rent).flat();
-              let dataRent = await Rent.bulkCreate(rent)
-          }
-          if(!infoDbRoom.length){
-              let room = data.map(e=>e.room).flat()
-              let dataRoom = await Room.bulkCreate(room)
-          }
-          return res.status(200).json("Base de datos CREADA!")
-
-    }
+    let infoDbType = await Type.findAll();
     
-    return res.status(200).json("Base de datos completa")
+    
+    if (!infoDB.length || !infoDbRent.length || !infoDbRoom.length ||!infoDbType.length) {
+       let clientes = data.map((e) => e.client).flat();
+      let dataClient = await Client.bulkCreate(clientes);
+
+      let rent = data.map((e) => e.rent).flat();
+      let dataRent = await Rent.bulkCreate(rent);
+
+      let room = data.map((e) => e.room).flat();
+      let dataRoom = await Room.bulkCreate(room);
+
+      let type = data.map((e) => e.type).flat();
+      let dataType = await Type.bulkCreate(type);
+     
+    }
+    // let idRoom = await Room.findByPk(Math.round(Math.random()*5))
+    // let idType = await Type.findByPk(Math.round(Math.random()))
+      
+    
+      //idType.addRoom(idRoom)
+    let idRoom
+    let idType 
+    async function repit(){
+      for(let i =1;i<6;i++){
+      idRoom = await Room.findByPk(Math.round(Math.random()*(5-1)+1))
+      idType = await Type.findByPk(Math.round(Math.random()*(2-1)+1))
+      await idType.addRoom(idRoom) 
+    }}
+    repit() 
+      
+       
+    
+   
+    
+  
+
+    return res.status(200).json("Base de datos completa");
   } catch (e) {
     res.status(400).json(e);
   }
 };
-
 
 module.exports = { loadInfoHostel };
