1420dc2c18e9fe2259ea9782196fb347362abecf
SophSawczuk <sofiasawczuka@gmail.com>
2022-11-12 18:37:45 -0300

    Component RoomCard styled with Boostrap

--- client/src/Components/rooms/Rooms.jsx
+++ client/src/Components/rooms/Rooms.jsx
@@ -21,30 +21,34 @@ export default function Rooms() {
     }
 
     return (
-        <div className={style.background}>
+        <div className={style.Container}>
             <h1><i>HABITACIONES</i></h1>
         <div className={style.Container}>
             
             <div className={style.Cards}>
             <div>
-                
                 <select id='type' defaultValue="Todo" onChange={(e) => roomTypeHandler(e)}>
                     <option value="Todo" hidden>Tipo de habitación</option>
                     <option value="Privado" >Privado</option>
                     <option value="Público" >Público</option>
                 </select>
             </div>
-            <div className={style.ContainerCards}>
+            <div className="container">
+                <div className="row">
             {allRooms && allRooms.map(e => {    
                 return(
-                    
+                    <div className="view  overlay">
                         <RoomCard
                             beds={e.beds} description={e.description} image={e.image} bathroom={e.bathroom} type={e.type}
-                            className={style.Card} id= {e.id}
+                            /* className=".hoverable" */ id= {e.id}
                         />  
+                    </div>
                 )  
+                
             
             })}
+                </div>
+           
             </div>
             <Footer />
             </div>
