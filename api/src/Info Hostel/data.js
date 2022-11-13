60f2ae42e47129ddbf89ca49dd4f6afb1184774b
zyro2930 <zyro2930@gmail.com>
2022-11-13 16:34:55 -0300

    correccion ruta info

--- api/src/Info Hostel/data.js
+++ api/src/Info Hostel/data.js
@@ -1,5 +1,10 @@
 module.exports = [
   {
+    type: [
+      { type: "Publico" },
+      { type: "Privado" },
+      
+    ],
     client: [
       {
         personalID: 1520,
@@ -34,14 +39,14 @@ module.exports = [
       {
         dateIn: new Date(2023, 00, 01),
         dateOut: new Date(2023, 00, 05),
-        price: 6500.50,
+        price: 6580.2,
         observation: "",
         status: true,
       },
       {
         dateIn: new Date(2023, 00, 05),
         dateOut: new Date(2023, 00, 10),
-        price: 3500,
+        price: 3500.5,
         observation: "",
         status: true,
       },
@@ -59,11 +64,12 @@ module.exports = [
         description: "Habitacion simple con 10 camas",
         image:
           "https://www.hostelclub.com/images/habitacion_compartidas_ppal.jpg",
-        bathroom: true,
+        bathroom: false,
 
         price: 3500.5,
         observation: "",
         status: true,
+        typeId: 2
       },
       {
         beds: 4,
@@ -75,6 +81,7 @@ module.exports = [
         price: 1995.5,
         observation: "",
         status: false,
+        typeId: 1
       },
 
       {
@@ -86,7 +93,30 @@ module.exports = [
         price: 2450.5,
         observation: "",
         status: true,
+        typeId: 1
+      },
+      {
+        beds: 1,
+        description: "Habitacion con cama doble",
+        image:
+          "https://www.pensionjoakina.com/public/images/sites/2198-135177.jpg",
+        bathroom: true,
+        price: 4550.5,
+        observation: "",
+        status: true,
+        typeId: 1
+      },
+      {
+        beds: 2,
+        description: "Habitacion con cama doble y una cama simple",
+        image:
+          "https://previews.123rf.com/images/rilueda/rilueda1604/rilueda160400155/55280236-cama-individual-y-doble-en-la-habitaci%C3%B3n-de-hotel.jpg",
+        bathroom: true,
+        price: 6550.5,
+        observation: "",
+        status: true,
+        typeId: 2
       },
     ],
-  },
+  },
 ];
