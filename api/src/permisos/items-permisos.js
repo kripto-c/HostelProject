66b376866ba93ef26ebac59bab21ebe0850136b3
kripto-c <mgutierrezxred@gmail.com>
2022-11-16 11:39:48 -0400

    permisos en prueba

---
+++ api/src/permisos/items-permisos.js
@@ -0,0 +1,9 @@
+// const jwtAuthz = require("express-jwt-authz");
+const ItemPermission = {
+  modifict : "modifict:hostellWeb"
+}
+
+module.exports ={
+  ItemPermission
+}
+
--- client/src/Components/login/Login.jsx
+++ client/src/Components/login/Login.jsx
@@ -14,12 +14,11 @@ async function callProtected() {
    // axios.get('http://localhost:4000/protected').then(responds => console.log(responds.data)).catch(e=> console.log(e))
   try {
    const token = await getAccessTokenSilently();
-   const responds = await axios.get('http://localhost:4000/login/protected', {
+   const responds = await axios.get('http://localhost:4000/', {
       headers:{
          authorization:`Bearer ${token}`
       }
    });
-
    console.log(responds.data);
   } catch (error) {
     console.log(error);
@@ -50,11 +49,11 @@ async function getProfile() {
 
            <h3>User is {isAuthenticated ? 'logged in ' : 'Not logged in'}</h3>
              
-          {/* <ul>
+          <ul>
             <li><button onClick={callApi}>Call Api route</button></li>
             <li><button onClick={callProtected}>Call Protected Api route</button></li>
             <li><button onClick={getProfile}>Get info profile</button></li>
-          </ul> */}
+          </ul>
 
             {
                isAuthenticated && <pre style={{textAlign: 'start'}}>{JSON.stringify(user, null, 2)}</pre>
