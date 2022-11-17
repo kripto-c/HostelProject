7cfb610684e789a70491c80b2593acd83d58d98c
kripto-c <mgutierrezxred@gmail.com>
2022-11-17 11:47:38 -0400

    add roles admin in route

---
+++ api/src/permisos/permisos.js
@@ -0,0 +1,6 @@
+module.exports = itemPermissos ={
+ addDataAdmin:"add:data-admin",	
+ clientRoute: "get_add_modifict:acces-route-client",
+ navGeneral:"navGeneral:view-hostel", 	
+ payment:"payment:reserve-hostel"
+}
