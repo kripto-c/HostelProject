44dcdb9527fc3fb851cca0fbee1f39b66442b262
kripto-c <mgutierrezxred@gmail.com>
2022-11-17 11:49:29 -0400

    update

--- client/src/App.js
+++ client/src/App.js
@@ -24,7 +24,6 @@ function App() {
         <Route path="/feedback" element={<FeedBack/>} />
         <Route path="/roomdetail/:id" element={<RoomDetail/>} />
         <Route path="/clientEdit" element={<ClientEdit />} />
-        <Route path="/prueba"  element={<Login />}/>
       </Routes>
     </>
   );
