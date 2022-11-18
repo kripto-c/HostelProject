a0499c21019a9bcd496a72a95e0fc384315892e0
german <haspenngames14@gmail.com>
2022-11-18 15:47:24 -0300

    mas retoques

--- client/src/Components/navbar/navbar.jsx
+++ client/src/Components/navbar/navbar.jsx
@@ -5,11 +5,11 @@ import NavDropdown from "react-bootstrap/NavDropdown";
 import { useAuth0 } from "@auth0/auth0-react";
 import { getCLient } from "../../Redux/actions";
 import axios from "axios";
-import { useDispatch, useSelector} from "react-redux";
-import {Link} from 'react-router-dom';
-import {useState, useEffect } from 'react'
-import './navbar.css'; 
-import logo from '../../images/logo.svg'
+import { useDispatch, useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
+import "./navbar.css";
+import logo from "../../images/logo.svg";
 function Navbars() {
   const {
     loginWithPopup,
@@ -18,9 +18,9 @@ function Navbars() {
     isAuthenticated,
     getAccessTokenSilently,
   } = useAuth0();
- const dispatch = useDispatch()
- const client = useSelector((state) => state.client);
-  const[view, setView] = useState(true);
+  const dispatch = useDispatch();
+  const client = useSelector((state) => state.client);
+  const [view, setView] = useState(true);
   const [confirmLog, setConfirmLog] = useState(false);
   const [Sort, setSort] = useState("");
 
@@ -34,53 +34,58 @@ function Navbars() {
         },
       });
       console.log(info.data);
-       getInfo()
+      getInfo();
       localStorage.setItem("IDUser", info.data.id);
     } catch (error) {
       console.log(error);
     }
   }
-  const verOptiones = ()=>{
-    const bar = document.querySelector('.nav')
-    if(view) {
-      setView(false)
-      return bar.style.height='max-content'
+  const verOptiones = () => {
+    const bar = document.querySelector(".nav");
+    if (view) {
+      setView(false);
+      return (bar.style.height = "max-content");
     } else {
       setView(true);
-      return bar.style.height='0'
-    };
+      return (bar.style.height = "0");
+    }
+  };
+
+  async function getInfo() {
+    const token = await getAccessTokenSilently();
+    dispatch(getCLient(token));
   }
 
-async function getInfo() {
-    const token = await getAccessTokenSilently()
-    dispatch(getCLient(token))
-}
-
-
-  useEffect(()=>{
+  useEffect(() => {
     let idUser = localStorage.getItem("IDUser");
-    if(idUser) {
-     getInfo()
+    if (idUser) {
+      getInfo();
       setConfirmLog(true);
-    };
-    if(client.length > 0 && isAuthenticated ){
-       getInfo()
     }
-},[dispatch])
+    if (client.length > 0 && isAuthenticated) {
+      getInfo();
+    }
+  }, [dispatch]);
 
   return (
     <>
       <Navbar variant="dark" bg="dark">
         <Container className="d-flex justify-content-between container-fluid">
-          <Link style={{textDecoration: "none"}} to="/" className="d-block">
+          <Link style={{ textDecoration: "none" }} to="/" className="d-block">
             {/* <h2 className="h5">Dinamita Hostel</h2> */}
-     {/* <div className="container-fluid "> */}
-    <div className="navbar-brand d-flex">    
-        <img src={logo} alt="Logo" width="50" height="50" className="d-inline-block align-text-top bg-light rounded-1" />
-        <p className="m-auto ms-3">Dinamita Hostel</p>
-    </div>
-  {/* </div> */}
-            </Link>
+            {/* <div className="container-fluid "> */}
+            <div className="navbar-brand d-flex">
+              <img
+                src={logo}
+                alt="Logo"
+                width="50"
+                height="50"
+                className="d-inline-block align-text-top bg-light rounded-1"
+              />
+              <p className="m-auto ms-3">Dinamita Hostel</p>
+            </div>
+            {/* </div> */}
+          </Link>
           <div className="res">
             <div className="but">
               <div className="act" onClick={verOptiones}>
@@ -90,60 +95,79 @@ async function getInfo() {
               </div>
             </div>
             <Nav className="nav">
-              <Link className="linkComponent" to="/ownerCrud">Tablero</Link>
-              <Link className="linkComponent" to="/rooms">Habitaciones</Link>
-              <Link className="linkComponent" to="/contact">Contactanos</Link>
-              <Link className="linkComponent" to="/about">Acerca de</Link>
-              {
-                confirmLog &&
-              <Link className="linkComponent" to="/reviewHostel">Reviews</Link>
-              }
+              <Link className="linkComponent" to="/ownerCrud">
+                Tablero
+              </Link>
+              <Link className="linkComponent" to="/rooms">
+                Habitaciones
+              </Link>
+              <Link className="linkComponent" to="/contact">
+                Contactanos
+              </Link>
+              <Link className="linkComponent" to="/about">
+                Acerca de
+              </Link>
+              {confirmLog && (
+                <div>
+                  <Link className="linkComponent" to="/reviewHostel">
+                    Reviews
+                  </Link>
+                  <Link className="linkComponent" to="/createRoom">
+                    Crear Habitacion
+                  </Link>
+                </div>
+              )}
             </Nav>
           </div>
           <>
-          {isAuthenticated ? (
-
-          
-             <Navbar.Collapse className="loginInfo">
-             <img src={isAuthenticated ? user.picture : ""} alt="foto perfil" className='rounded-circle profileIMG'/>
-              <Nav className="me-auto CuentaLog">
-                <button className="miCuentaOp">Mi Cuenta</button>
+            {isAuthenticated ? (
+              <Navbar.Collapse className="loginInfo">
+                <img
+                  src={isAuthenticated ? user.picture : ""}
+                  alt="foto perfil"
+                  className="rounded-circle profileIMG"
+                />
+                <Nav className="me-auto CuentaLog">
+                  <button className="miCuentaOp">Mi Cuenta</button>
                   <div className="LoginOp">
-                  <Link to="/clientEdit" className="clientEdit">
-                    Editar Datos
-                  </Link>
-                  <NavDropdown.Item href="#action/3.2">
-                    Registro
-                  </NavDropdown.Item>
-                  <NavDropdown.Divider />
-                  <NavDropdown.Item href="#action/3.4" onClick={() =>{ 
-                    localStorage.clear(); 
-                    logout(); 
-                    setConfirmLog(false);
-                    setSort("deslogueado")
-                    }}>
-                    cerrar sesión
-                  </NavDropdown.Item>
+                    <Link to="/clientEdit" className="clientEdit">
+                      Editar Datos
+                    </Link>
+                    <NavDropdown.Item href="#action/3.2">
+                      Registro
+                    </NavDropdown.Item>
+                    <NavDropdown.Divider />
+                    <NavDropdown.Item
+                      href="#action/3.4"
+                      onClick={() => {
+                        localStorage.clear();
+                        logout();
+                        setConfirmLog(false);
+                        setSort("deslogueado");
+                      }}
+                    >
+                      cerrar sesión
+                    </NavDropdown.Item>
                   </div>
-              </Nav>
-            </Navbar.Collapse>
-          ) : (
-            <Link
-              className="Login"
-              to='/#login'
-              onClick={async (e) => {
-                e.preventDefault()
-                await loginWithPopup();
-                setClient();
-                setConfirmLog(true);
-              }}
-           >
-              Login
-            </Link>
-          )}
-    </> 
+                </Nav>
+              </Navbar.Collapse>
+            ) : (
+              <Link
+                className="Login"
+                to="/#login"
+                onClick={async (e) => {
+                  e.preventDefault();
+                  await loginWithPopup();
+                  setClient();
+                  setConfirmLog(true);
+                }}
+              >
+                Login
+              </Link>
+            )}
+          </>
         </Container>
-      </Navbar>         
+      </Navbar>
     </>
   );
 }
