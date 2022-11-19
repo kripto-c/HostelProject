943a7ffae16811b5f064f89c994c79504441898c
galoss88 <gallogarcianicolas@gmail.com>
2022-11-19 17:14:55 -0300

    clients

--- api/src/routes/numberClients/getAllClients.js
+++
@@ -1,8 +0,0 @@
-const {Router} = require("express");
-const getAllClients = require("../../controllers/getAllClients.js")
-const router = Router();
-
-router.get("/",getAllClients)
-
-
-module.exports=router;
