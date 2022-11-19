a42cb3d54d60b3832a088b62e3ed081da72f34a4
galoss88 <gallogarcianicolas@gmail.com>
2022-11-19 10:54:52 -0300

    dashboard, avances

--- client/src/Components/Dashboard/FAQ.jsx
+++ client/src/Components/Dashboard/FAQ.jsx
@@ -9,21 +9,21 @@ export default function FAQ() {
   const faqs = [
     {
       icon: <AiFillCalendar />,
-      text: "How to manage time and get good marks for trips",
+      text: "FALTA",
     },
     {
       icon: <MdTimelapse />,
-      text: "How to regulate transactions over time",
+      text: "FALTA",
     },
     {
       icon: <IoMdCash />,
-      text: "Withdrawing money through an ATM",
+      text: "FALTA",
     },
   ];
   return (
     <Section>
       <div className="title">
-        <h2>Information for drivers</h2>
+        <h2>FAQ</h2>
       </div>
       <div className="faqs">
         {faqs.map((faq) => {
