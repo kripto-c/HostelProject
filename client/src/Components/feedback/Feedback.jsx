ecc567f89045c7af28ad987faa5d8599bda4691b
LuCardozo <lucianocar12300@gmail.com>
2022-11-21 12:38:32 -0300

    mod

--- client/src/Components/feedback/Feedback.jsx
+++ client/src/Components/feedback/Feedback.jsx
@@ -1,13 +1,12 @@
 import React from "react";
 import { useEffect } from "react";
-import  { Link, useNavigate } from "react-router-dom"
+import  { Link } from "react-router-dom"
 import { useDispatch } from "react-redux";
 import { sendFeedback } from "../../Redux/actions";
 import s from "./feedback.module.css"
 export default function FeedBack(){
     const dispatch = useDispatch();
     const queryString = window.location.search;
-    const naviGate = useNavigate();
 
     useEffect(() =>{
 
