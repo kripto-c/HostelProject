2ea9ac264b0aaedeab1d8ff63296768c5bdc4b6a
kripto-c <mgutierrezxred@gmail.com>
2022-11-24 15:08:51 -0400

    add faqs route admin delete and create

--- api/src/routes/faqs/route.js
+++ api/src/routes/faqs/route.js
@@ -1,13 +1,14 @@
 const { Router} = require('express');
-const { postFaq, getFaq, updateFaq, deleteFaq } = require('./controller');
+const { postFaq, getFaq, deleteFaq} = require('./controller');
 const route = Router();
 
 route.get('/', getFaq)
 
 route.post('/new', postFaq);
 
-route.post('/faqEdit', updateFaq)
+// route.post('/faqEdit', updateFaq)
 
 route.get('/deleteFaq', deleteFaq)
 
+// route.get('/faqSearch', searchFaq)
 module.exports = route;
