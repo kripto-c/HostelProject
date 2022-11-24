2fe32c95f5555db17a05a996936c57b81d97718e
kripto-c <mgutierrezxred@gmail.com>
2022-11-24 10:23:45 -0400

    add component Faqs admin

---
+++ client/src/Components/Dashboard/PanelAdmin.jsx
@@ -0,0 +1,49 @@
+import React, { useEffect } from "react";
+import { Route, Routes, useNavigate } from "react-router-dom";
+import Create from "./Create";
+import OwnerCrud from "../OwnerCrud";
+import Dashboard from "./Dashboard";
+import Payments from "./Payments";
+import ReviewAdmin from "./ReviewAdmin";
+import Sidebar from "./Sidebar";
+import { useState } from "react";
+import Offcanvas from "react-bootstrap/Offcanvas";
+import Faqs from "./Faqs";
+export default function PanelAdmin() {
+  const navigate= useNavigate();
+  useEffect(()=>navigate("/admin/dashboard"),[])
+  const [show, setShow] = useState(false);
+  const handleShow = () => setShow(true);
+  return (
+    <React.Fragment>
+      <div>
+      
+        {
+          console.log(show)
+        }
+        <Offcanvas show={show} onHide={() =>{setShow(false)}} style={{backgroundColor: "#212121"}}>
+          <Sidebar></Sidebar>
+        </Offcanvas>
+          <Routes>
+            <Route path="dashboard" element={<Dashboard></Dashboard>}></Route>
+          </Routes>
+          <Routes>
+            <Route path="reviewsAdmin" element={<ReviewAdmin></ReviewAdmin>}></Route>
+          </Routes>
+          <Routes>
+            <Route path="roomsAdmin" element={<Create></Create>}></Route>
+          </Routes>
+          <Routes>
+            <Route path="payments" element={<Payments></Payments>}></Route>
+          </Routes>
+          <Routes>
+            <Route path="settings" element={<OwnerCrud></OwnerCrud>}></Route>
+          </Routes>
+          <Routes>
+            <Route path="faqs" element={<Faqs/>}></Route>
+          </Routes>
+      </div>
+    </React.Fragment>
+  );
+}
+
---
+++ client/src/Components/Dashboard/Sidebar.jsx
@@ -0,0 +1,380 @@
+import React, { useState, useEffect } from "react";
+import { Routes, Route } from "react-router-dom";
+import { MdSpaceDashboard } from "react-icons/md";
+import { RiDashboard2Fill } from "react-icons/ri";
+import { FaAddressCard } from "react-icons/fa";
+import { GiTwirlCenter } from "react-icons/gi";
+import { BsFillChatTextFill } from "react-icons/bs";
+import { IoSettings } from "react-icons/io5";
+import { FiLogOut } from "react-icons/fi";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { VscChromeClose } from "react-icons/vsc";
+import scrollreveal from "scrollreveal";
+import logo from "../../images/logo.svg";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+import Dashboard from "./Dashboard";
+import Payments from "./Payments.jsx";
+
+export default function Sidebar() {
+  const [currentLink, setCurrentLink] = useState(1);
+  const [navbarState, setNavbarState] = useState(false);
+  const html = document.querySelector("html");
+  html.addEventListener("click", () => setNavbarState(false));
+
+  // useEffect(() => {
+  //   const sr = scrollreveal({
+  //     origin: "left",
+  //     distance: "80px",
+  //     duration: 1000,
+  //     reset: false,
+  //   });
+
+  //   sr.reveal(
+  //     `
+  //     .brand,
+  //     .links>ul>li:nth-of-type(1),
+  //     .links>ul>li:nth-of-type(2),
+  //     .links>ul>li:nth-of-type(3),
+  //     .links>ul>li:nth-of-type(4),
+  //     .links>ul>li:nth-of-type(5),
+  //     .links>ul>li:nth-of-type(6),
+  //     .logout
+  //     `,
+  //     {
+  //       opacity: 0,
+  //       interval: 300,
+  //     }
+  //   );
+  // }, []);
+
+  return (
+    <div>
+      <div className="container">
+        <Section>
+          {/* //TOP-------------------------------------------->> */}
+          <div className="top">
+            <div className="brand">
+              <img className="logo" src={logo} />
+              <span>Project Hostel</span>
+            </div>
+            {/* //TOGGLE-------------------------------------------->> */}
+            {/* <div className="toggle">
+              {navbarState ? (
+                <VscChromeClose onClick={() => setNavbarState(false)} />
+              ) : (
+                <GiHamburgerMenu
+                  onClick={(e) => {
+                    e.stopPropagation();
+                    setNavbarState(true);
+                  }}
+                />
+              )}
+            </div> */}
+            <div className="links">
+              <ul>
+                <li
+                  className={currentLink === 1 ? "active" : "none"}
+                  onClick={() => setCurrentLink(1)}
+                >
+                  {/* <Routes>
+                  <Route path="dashboard" element={<Dashboard/>}></Route>
+                </Routes> */}
+                  <Link to="/admin/dashboard">
+                    <MdSpaceDashboard />
+                    <span> Dashboard</span>
+                  </Link>
+                </li>
+                <li
+                  className={currentLink === 2 ? "active" : "none"}
+                  onClick={() => setCurrentLink(2)}
+                >
+                  <Link to="/admin/reviewsAdmin">
+                    <MdSpaceDashboard />
+                    <span> Reviews</span>
+                  </Link>
+                </li>
+                <li
+                  className={currentLink === 3 ? "active" : "none"}
+                  onClick={() => setCurrentLink(3)}
+                >
+                  {" "}
+                  <Link to="/admin/roomsAdmin">
+                    <MdSpaceDashboard />
+                    <span> Rooms</span>
+                  </Link>
+                </li>
+                <li
+                  className={currentLink === 4 ? "active" : "none"}
+                  onClick={() => setCurrentLink(4)}
+                >
+                  <Link to="/admin/payments">
+                    <MdSpaceDashboard />
+                    <span> Pagos </span>
+                  </Link>
+                </li>
+
+                {/* <li
+                className={currentLink === 5 ? "active" : "none"}
+                onClick={() => setCurrentLink(5)}>
+                <a href="#">
+         
+                  <span> FAQs</span>
+                </a>
+              </li> */}
+                      <li
+                  className={currentLink === 5 ? "active" : "none"}
+                  onClick={() => setCurrentLink(5)}
+                >
+                  <Link to="/admin/faqs">
+                  <BsFillChatTextFill />
+                    <span> FaQs</span>
+                  </Link>
+                </li>
+
+                <li
+                  className={currentLink === 6 ? "active" : "none"}
+                  onClick={() => setCurrentLink(6)}
+                >
+                  <Link to="/admin/settings">
+                    <IoSettings />
+                    <span> Settings </span>
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div className="logout">
+            <a href="#">
+              <FiLogOut />
+              <span className="logout">Logout</span>
+            </a>
+          </div>
+        </Section>
+        <ResponsiveNav
+          state={navbarState}
+          className={navbarState ? "show" : ""}
+        >
+          <div className="responsive__links">
+            <ul>
+              <li
+                className={currentLink === 1 ? "active" : "none"}
+                onClick={() => setCurrentLink(1)}
+              >
+                <a href="#">
+                  <MdSpaceDashboard />
+                  <span> Dashboard</span>
+                </a>
+              </li>
+              <li
+                className={currentLink === 2 ? "active" : "none"}
+                onClick={() => setCurrentLink(2)}
+              >
+                <a href="#">
+                  <RiDashboard2Fill />
+                  <span> Riders</span>
+                </a>
+              </li>
+              <li
+                className={currentLink === 3 ? "active" : "none"}
+                onClick={() => setCurrentLink(3)}
+              >
+                <a href="#">
+                  <FaAddressCard />
+                  <span> Payment Details</span>
+                </a>
+              </li>
+              <li
+                className={currentLink === 4 ? "active" : "none"}
+                onClick={() => setCurrentLink(4)}
+              >
+                <a href="/admin/createRoom">
+                  <GiTwirlCenter />
+                  <span> Learning Center</span>
+                </a>
+              </li>
+              <li
+                className={currentLink === 5 ? "active" : "none"}
+                onClick={() => setCurrentLink(5)}
+              >
+                <a href="#">
+                  <BsFillChatTextFill />
+                  <span> FAQs</span>
+                </a>
+              </li>
+              <li
+                className={currentLink === 6 ? "active" : "none"}
+                onClick={() => setCurrentLink(6)}
+              >
+                <a href="#">
+                  <IoSettings />
+                  <span> Settings</span>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </ResponsiveNav>
+      </div>
+    </div>
+  );
+}
+const Section = styled.section`
+  position: fixed;
+  left: 0.5%;
+  background-color: #212121;
+  height: 120vh;
+  width: 32vw;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+  padding: 2rem 0;
+  gap: 3rem;
+  .logo {
+    width: 60px;
+  }
+  .top {
+    display: flex;
+    flex-direction: column;
+    gap: 2rem;
+    width: 100%;
+    .toggle {
+      display: none;
+    }
+    .brand {
+      width: 100%;
+      display: flex;
+
+      justify-content: center;
+      align-items: center;
+      gap: 2rem;
+      svg {
+        color: #ffc107;
+        font-size: 2rem;
+      }
+      span {
+        font-size: 2rem;
+        color: #ffc107;
+        font-family: "Permanent Marker", cursive;
+      }
+    }
+    .links {
+      width: 100%;
+      display: flex;
+      justify-content: center;
+      ul {
+        list-style-type: none;
+        display: flex;
+        flex-direction: column;
+        gap: 1rem;
+        li {
+          padding: 0.6rem 1rem;
+
+          border-radius: 0.6rem;
+          &:hover {
+            background-color: white;
+            a {
+              color: black;
+            }
+          }
+          a {
+            text-decoration: none;
+            display: flex;
+            gap: 1rem;
+            color: white;
+          }
+        }
+        .active {
+          width: 100%;
+          background-color: white;
+          a {
+            color: black;
+          }
+        }
+      }
+    }
+  }
+  .logout {
+    padding: 0.3rem 1rem;
+    border-radius: 0.6rem;
+    &:hover {
+      background-color: #da0037;
+    }
+    a {
+      text-decoration: none;
+      display: flex;
+      align-items: center;
+      justify-content: flex-start;
+      color: white;
+    }
+  }
+  @media screen and (min-width: 280px) and (max-width: 1080px) {
+    position: initial;
+    width: 100%;
+    height: max-content;
+    padding: 1rem;
+    .top {
+      flex-direction: row;
+      align-items: center;
+      justify-content: space-between;
+      padding: 0 1rem;
+      .toggle {
+        display: block;
+        color: white;
+        z-index: 99;
+        svg {
+          font-size: 1.4rem;
+        }
+      }
+      .brand {
+        gap: 1rem;
+        justify-content: flex-start;
+      }
+    }
+    .top > .links,
+    .logout {
+      display: none;
+    }
+  }
+`;
+
+const ResponsiveNav = styled.div`
+  position: fixed;
+  right: -10vw;
+  top: 0;
+  z-index: 10;
+  background-color: white;
+  height: 100vh;
+  width: ${({ state }) => (state ? "60%" : "0%")};
+  transition: 0.4s ease-in-out;
+  display: flex;
+  opacity: 0;
+  visibility: hidden;
+  padding: 1rem;
+  .responsive__links {
+    width: 100%;
+    ul {
+      list-style-type: none;
+      display: flex;
+      flex-direction: column;
+      gap: 1rem;
+      margin-top: 3rem;
+      width: 100%;
+      li {
+        padding: 0.6rem 1rem;
+        width: 100%;
+        border-radius: 0.6rem;
+        &:hover {
+          background-color: white;
+        }
+      }
+      .active {
+        width: 100%;
+        background-color: white;
+        a {
+          color: black;
+        }
+      }
+    }
+  }
+`;
