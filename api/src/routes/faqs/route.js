35720546f41f7252297c176c36fdb805f33fc516
kripto-c <mgutierrezxred@gmail.com>
2022-11-24 17:30:51 -0400

    protected route faq

--- api/src/routes/faqs/route.js
+++ api/src/routes/faqs/route.js
@@ -1,14 +1,13 @@
 const { Router} = require('express');
 const { postFaq, getFaq, deleteFaq} = require('./controller');
+const checkPermissions = require("../../permisos/permisosCheck");
+const itemPermissos = require('../../permisos/permisos')
 const route = Router();
 
 route.get('/', getFaq)
 
-route.post('/new', postFaq);
+route.post('/new', checkPermissions(itemPermissos.faq),postFaq);
 
-// route.post('/faqEdit', updateFaq)
+route.get('/deleteFaq', checkPermissions(itemPermissos.faqDelete),deleteFaq)
 
-route.get('/deleteFaq', deleteFaq)
-
-// route.get('/faqSearch', searchFaq)
 module.exports = route;
