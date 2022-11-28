ffa522faefd5970d12ddf7235b2880adcd49246a
galoss88 <gallogarcianicolas@gmail.com>
2022-11-28 13:04:14 -0300

    arreglado espacios en chatbot

--- client/src/Components/chatbotTwo/MensajeInicial.jsx
+++ client/src/Components/chatbotTwo/MensajeInicial.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function MensajeInicial(props) {
     const {steps}=props
     const {preguntaNombre} = steps
-    const nombreCurado = preguntaNombre.value.split(" ").map(e=>e[0].toUpperCase()+e.slice(1)).join(" ")
+    const nombreCurado = preguntaNombre.value.trim().split(" ").map(e=>e[0].toUpperCase()+e.slice(1)).join(" ")
   return (
     <div>
         <p>Hola {nombreCurado}, Bienvenido a Dinamita Hostel!!!</p>
