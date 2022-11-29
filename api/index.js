f831845880289a700fa0f0d27c1b5efd184b73c0
ToledoFernando <elvioladorxx3@gmail.com>
2022-11-29 16:28:49 -0300

    modal mercado pago

--- api/index.js
+++ api/index.js
@@ -20,7 +20,7 @@
 const serverHttp = require("./src/app");
 const { conn } = require("./src/db.js");
 
-conn.sync({ force: false}).then(() => {
+conn.sync({ force: true }).then(() => {
   serverHttp.listen(process.env.PORT || 4000, () => {
     console.log(`server ready en el puerto ${process.env.PORT || 4000}`);
   });
