89db6821a644d41262acb21f5265a5de32313cf8
galoss88 <gallogarcianicolas@gmail.com>
2022-12-01 16:41:22 -0300

    Update index.js

--- client/src/Redux/actions/index.js
+++ client/src/Redux/actions/index.js
@@ -12,8 +12,8 @@ export const GET_FAQ = "GET_FAQ";
 export const FORGET = "FORGET";
 
 // const URL = "https://dinamitahostel.herokuapp.com"; //heroku
- const URL = "https://hostelproject-production.up.railway.app"; //railway
-//const URL = "http://localhost:4000"; //descomentar para hacer pruebas
+  const URL = "https://hostelproject-production.up.railway.app"; //railway
+// const URL = "http://localhost:4000"; //descomentar para hacer pruebas
 
 //ACTION ROOMS ----------------------------------------------------------->>
 export function getRooms() {
