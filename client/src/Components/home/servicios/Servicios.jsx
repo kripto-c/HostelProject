55d2a07885f6b6f628015946cdc9b5b48fbb9ce7
galoss88 <gallogarcianicolas@gmail.com>
2022-11-30 21:16:17 -0300

    cambios servicios

--- client/src/Components/home/servicios/Servicios.jsx
+++ client/src/Components/home/servicios/Servicios.jsx
@@ -11,28 +11,32 @@ import Tv from "./Tv";
 export default function Servicios() {
   return (
     <div className="container-fluid">
-      <h1>Servicios</h1>
+      
 
       <Container className="divServicios">
+     
         <Row>
+          <Col className="m-auto mt-4">
+          <h1>Servicios</h1>
+          </Col>
           
           <Col className="">
             <Wifi></Wifi>
-            <div className="container-fluid w-50 ms-2"><small className="text"> Wifi <strong>Gratis</strong></small></div>
+            <div className="container-fluid w-50 ms-2"><small className="text"> Wifi Gratis</small></div>
             
           </Col>
           
           <Col>
             <Recepcion></Recepcion>
-            <div className="container-fluid w-50 ms-2"><small className="text"><strong>Recepción</strong></small></div>
+            <div className="container-fluid w-50 ms-2"><small className="text">Recepción</small></div>
           </Col>
           <Col>
             <Lavanderia></Lavanderia>
-            <div className="container-fluid w-50 ms-2"><small className="text">Servicio de <strong>Lavanderia</strong></small></div>
+            <div className="container-fluid w-50 ms-2"><small className="text">Servicio de Lavanderia</small></div>
           </Col>
           <Col>
             <Tv />
-            <div className="container-fluid w-75 align-items-center ms-0 "><small className="text"><strong>TV </strong>en sala común</small></div>
+            <div className="container-fluid w-75 align-items-center ms-0 "><small className="text">TV en sala común</small></div>
           </Col>
         </Row>
       </Container>
